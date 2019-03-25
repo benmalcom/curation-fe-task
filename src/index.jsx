b58bba761acecbddea52618e125696b06d8a3b11
Benjamin Ikeji <benjaminikeji@gmail.com>
2019-03-25 17:19:13 +0100

    Refactor and create index.jsx file

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+
+ReactDOM.render(<App
+		title="Personal Card"
+		subHead="Lorem ipsum dolor sit amet, consectetur adipiscing elit"/>,
+		document.getElementById('root'));
