58f37fafd0c719a5e1a3783fddd0aca650f9c80b
Benjamin Ikeji <benjaminikeji@gmail.com>
2019-03-25 16:50:37 +0100

    Add App component as the base component with styles

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+import PropTypes from 'prop-types';
+import TabbedContent from '../../components/TabbedContent';
+import PersonalInformation from '../../components/PersonalInformation';
+import Notes from '../../components/Notes';
+
+import user1Img from '../../assets/images/user.png';
+import user2Img from '../../assets/images/user@2x.png';
+import user3Img from '../../assets/images/user@3x.png';
+
+const App = ({ title, subHead}) => (
+		<div className="container">
+				<Row>
+						<Col lg={6} className="col-lg-6 p-card">
+								<h1>{title}</h1>
+								<p>{subHead}</p>
+								<PersonalInformation
+										title="Personal Information"
+										dob="25.10.1985"
+										email="diego.morata@domain.co.uk"
+										images={[user1Img, `${user2Img} 2x`, `${user3Img} 3x`]}
+								/>
+								<TabbedContent tabHeadings={[
+										{name: 'Address', id: 1},
+										{name: 'Subscribe', id: 2}]
+								}/>
+						</Col>
+						<Col lg={5} className="p-card right">
+								<Notes
+										title="Notes"
+										content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitursagittis sit amet sem eget gravida.
+										Duis tellus est, pharetra ac bibendum eu, consequat sed dolor. Duis mollis, augue adignissim
+										congue, mi felis interdum sapien, nec pellentesque eros erat sed diam. Sed pharetra lobortis interdum. "
+								/>
+						</Col>
+				</Row>
+		</div>
+);
+App.propTypes = {
+		title: PropTypes.string.isRequired,
+		subHead: PropTypes.string.isRequired,
+};
+export default App;
